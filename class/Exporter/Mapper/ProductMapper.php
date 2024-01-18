84110b7583cf06b9005eadea6fed0b43ffb92a63
James Collings <james@jclabs.co.uk>
2024-01-18 23:31:13 +0000

    Add missing product fields, downloadable files, fix product gallery

--- class/Exporter/Mapper/ProductMapper.php
+++ class/Exporter/Mapper/ProductMapper.php
@@ -11,6 +11,7 @@ class ProductMapper extends PostMapper
         parent::__construct($post_type);
 
         add_filter('iwp/exporter/post_type/custom_field_list',  [$this, 'remove_custom_fields'], 10, 2);
+        add_filter('iwp/exporter/post_type/fields', [$this, 'remove_taxonomy_fields'], 10, 2);
     }
 
     public function get_fields()
@@ -36,6 +37,34 @@ class ProductMapper extends PostMapper
                 'length',
                 'width',
                 'height',
+                'product_type',
+                'weight',
+                'length',
+                'width',
+                'height',
+                'total_sales',
+                'tax_status',
+                'tax_class',
+                'visibility',
+                'manage_stock',
+                'backorders',
+                'sold_individually',
+                'virtual',
+                'downloadable',
+                'download_limit',
+                'download_expiry',
+                'stock',
+                'stock_status',
+                'low_stock_amount',
+                'average_rating',
+                'review_count',
+                'regular_price',
+                'sale_price',
+                'date_on_sale_from',
+                'date_on_sale_to',
+                'purchase_note',
+                'product_url',
+                'button_text',
             ],
             'children' => []
         ];
@@ -112,6 +141,17 @@ class ProductMapper extends PostMapper
             'children' => []
         ];
 
+        // Downloadable files
+        $fields['children']['downloadable_files'] = [
+            'key' => 'downloadable_files',
+            'label' => 'Downloadable',
+            'loop' => false,
+            'fields' => [
+                'name', 'file'
+            ],
+            'children' => []
+        ];
+
         return $fields;
     }
 
@@ -129,7 +169,41 @@ class ProductMapper extends PostMapper
             'length' => $product->get_length(),
             'width' => $product->get_width(),
             'height' => $product->get_height(),
+            'total_sales' => $product->get_total_sales(),
+            'tax_status' => $product->get_tax_status(),
+            'tax_class' => $product->get_tax_class(),
+            'visibility' => $product->get_catalog_visibility(),
+            'manage_stock' => $product->get_manage_stock(),
+            'backorders' => $product->get_backorders(),
+            'sold_individually' => $product->get_sold_individually(),
+            'virtual' => $product->get_virtual(),
+            'downloadable' => $product->get_downloadable(),
+            'download_limit' => $product->get_download_limit(),
+            'download_expiry' => $product->get_download_expiry(),
+            'stock' => $product->get_stock_quantity(),
+            'stock_status' => $product->get_stock_status(),
+            'low_stock_amount' => $product->get_low_stock_amount(),
+            'average_rating' => $product->get_average_rating(),
+            'review_count' => $product->get_review_count(),
+            'regular_price' => $product->get_regular_price(),
+            'sale_price' => $product->get_sale_price(),
+            'date_on_sale_from' => $product->get_date_on_sale_from(),
+            'date_on_sale_to' => $product->get_date_on_sale_to(),
+            'purchase_note' => $product->get_purchase_note(),
+            'product_url' => '',
+            'button_text' => ''
         ];
+
+        if (is_a($product, '\WC_Product_External')) {
+
+            /**
+             * @var \WC_Product_External $product
+             */
+            $this->record['woocommerce'] = array_merge($this->record['woocommerce'], [
+                'product_url' => $product->get_product_url(),
+                'button_text' => $product->get_button_text(),
+            ]);
+        }
 
         // core product sku, added here to show in unique identifier field
         $this->record['sku'] = $product->get_sku();
@@ -143,51 +217,46 @@ class ProductMapper extends PostMapper
         // Product attributes
         $this->record['product_attributes'] = [];
 
-        $tmp = [];
         $attributes = $product->get_attributes();
-        foreach ($attributes as $attribute_id => $attribute_data) {
+        if (!empty($attributes)) {
+            $tmp = [];
+            foreach ($attributes as $attribute_id => $attribute_data) {
 
-            /**
-             * @var \WC_Product_Attribute|string $attribute_data
-             */
+                /**
+                 * @var \WC_Product_Attribute|string $attribute_data
+                 */
 
-            if ($attribute_data instanceof \WC_Product_Attribute) {
+                if ($attribute_data instanceof \WC_Product_Attribute) {
 
-                // returns int[]
-                // $tmp[$attribute_id] = $attribute_data->get_options();
+                    $tmp[sprintf('%s::name', $attribute_id)] = '';
+                    $tmp[sprintf('%s::visible', $attribute_id)] = $attribute_data->get_visible() ? 'yes' : 'no';
+                    $tmp[sprintf('%s::variation', $attribute_id)] = $attribute_data->get_variation() ? 'yes' : 'no';
 
-                // $tmp[$attribute_id] = [
-                $tmp[sprintf('%s::name', $attribute_id)] = '';
-                $tmp[sprintf('%s::visible', $attribute_id)] = $attribute_data->get_visible() ? 'yes' : 'no';
-                $tmp[sprintf('%s::variation', $attribute_id)] = $attribute_data->get_variation() ? 'yes' : 'no';
-                // 'visible' => $attribute_data->get_visible() ? 'yes' : 'no',
-                // 'variation' => $attribute_data->get_variation() ? 'yes' : 'no',
-                // ];
+                    $term_ids = $attribute_data->get_options();
 
-                $term_ids = $attribute_data->get_options();
+                    if (!empty($term_ids)) {
 
-                if (!empty($term_ids)) {
+                        if (taxonomy_exists($attribute_id)) {
 
-                    if (taxonomy_exists($attribute_id)) {
+                            foreach ($term_ids as $term_id) {
+                                $term = get_term_by('term_id', $term_id, $attribute_id);
+                                if (!$term) {
+                                    continue;
+                                }
 
-                        foreach ($term_ids as $term_id) {
-                            $term = get_term_by('term_id', $term_id, $attribute_id);
-                            if (!$term) {
-                                continue;
+                                $tmp[sprintf('%s::name', $attribute_id)] = $term->name;
                             }
-
-                            $tmp[sprintf('%s::name', $attribute_id)] = $term->name;
+                        } else {
+                            $tmp[sprintf('%s::name', $attribute_id)] = $term_ids;
                         }
-                    } else {
-                        $tmp[sprintf('%s::name', $attribute_id)] = $term_ids;
                     }
+                } else {
+                    $tmp[sprintf('%s::name', $attribute_id)] = $attribute_data;
                 }
-            } else {
-                $tmp[sprintf('%s::name', $attribute_id)] = $attribute_data;
             }
-        }
 
-        $this->record['product_attributes'] = $tmp;
+            $this->record['product_attributes'] = $tmp;
+        }
 
         // Linked Products
         $this->record['linked_products'] = [];
@@ -213,19 +282,41 @@ class ProductMapper extends PostMapper
         $gallery = $product->get_gallery_image_ids();
         $this->record['product_gallery'] = [];
 
-        foreach ($gallery as $thumbnail_id) {
+        if (!empty($gallery)) {
+            foreach ($gallery as $thumbnail_id) {
 
-            $attachment = get_post($thumbnail_id, ARRAY_A);
-            $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+                $attachment = get_post($thumbnail_id, ARRAY_A);
+                $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
 
-            $this->record['product_gallery'][] = [
-                'id' => $thumbnail_id,
-                'url' => wp_get_attachment_url($thumbnail_id),
-                'title' => $attachment['post_title'],
-                'alt' => $alt,
-                'caption' => $attachment['post_excerpt'],
-                'description' => $attachment['post_content']
-            ];
+                $product_gallery[] = [
+                    'id' => $thumbnail_id,
+                    'url' => wp_get_attachment_url($thumbnail_id),
+                    'title' => $attachment['post_title'],
+                    'alt' => $alt,
+                    'caption' => $attachment['post_excerpt'],
+                    'description' => $attachment['post_content']
+                ];
+            }
+
+            $this->record['product_gallery'] = $product_gallery;
+        }
+
+        // Downloadable files
+        /**
+         * @var \WC_Product_Download[] $downloads
+         */
+        $downloads = $product->get_downloads();
+        $this->record['downloadable_files'] = [];
+        if (!empty($downloads)) {
+
+            $downloadable_files = [];
+            foreach ($downloads as $download) {
+                $downloadable_files[] = [
+                    'name' => $download->get_name(),
+                    'file' => $download->get_file()
+                ];
+            }
+            $this->record['downloadable_files'] = $downloadable_files;
         }
 
         return $is_setup;
@@ -299,11 +390,38 @@ class ProductMapper extends PostMapper
             '_product_attributes',
             '_variation_description',
             'attribute_pa_colour',
+            '_downloadable_files',
         ];
 
         $fields = array_filter($fields, function ($item) use ($cf_fields_to_remove) {
             return !in_array($item, $cf_fields_to_remove);
         });
+
+        return $fields;
+    }
+
+    /**
+     * Hide woocommerce attribute taxonomies from taxonomy list
+     * 
+     * @param string[] $fields 
+     * @param string[] $post_types 
+     * @return string[] 
+     */
+    public function remove_taxonomy_fields($fields, $post_types)
+    {
+        if ($post_types !== $this->post_type) {
+            return $fields;
+        }
+
+        $attribute_taxonomies = wc_get_attribute_taxonomy_names();
+
+        foreach ($attribute_taxonomies as $attribute_taxonomy) {
+
+            $key = sprintf('tax_%s', $attribute_taxonomy);
+            if (isset($fields['children'][$key])) {
+                unset($fields['children'][$key]);
+            }
+        }
 
         return $fields;
     }
